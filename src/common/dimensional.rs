643576c7b6f5410ae2e8241adeca6d97da6a6a0d
René Perschon <rperschon85@gmail.com>
2016-12-31 12:00:36 +0100

    added doc for Dimensional

--- src/common/dimensional.rs
+++ src/common/dimensional.rs
@@ -1,5 +1,6 @@
 use ::Dimension;
 
+/// Denotes a struct that has a width and height of type `Dimension`.
 pub trait Dimensional {
     /// Returns the width of this object.
     fn width(&self) -> Dimension;
